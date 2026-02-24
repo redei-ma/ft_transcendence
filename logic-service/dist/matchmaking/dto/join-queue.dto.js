072d716b47849d9f2f389a7fd43bda0554814fc3
automa-KONG <72254805+automa-KONG@users.noreply.github.com>
2026-02-24 01:31:37 +0100

    Delete logic-service directory

--- logic-service/dist/matchmaking/dto/join-queue.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JoinQueueDto = void 0;
-class JoinQueueDto {
-    userId;
-    rank;
-}
-exports.JoinQueueDto = JoinQueueDto;
-//# sourceMappingURL=join-queue.dto.js.map
