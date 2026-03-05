8f0207d8747f84ca638f35c93f4e5aeae3d04bdb
giopiro99 <giovannipirozzi12345@gmail.com>
2026-03-05 13:19:10 +0100

    jwt token for game-service

--- game-service/src/modules/game/game.controller.ts
+++ game-service/src/modules/game/game.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '@transcendence/auth';
-import type { AuthenticatedRequest } from '@transcendence/auth';
+import { JwtAuthGuard, AuthenticatedRequest } from '@transcendence/auth';
 
 @Controller('game')
 @UseGuards(JwtAuthGuard)
