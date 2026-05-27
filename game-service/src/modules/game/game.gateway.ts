f20f78838d22abf2b4f35cc14e3d914f44524bd6
giopiro99 <giovannipirozzi12345@gmail.com>
2026-05-27 19:08:03 +0200

    aggiunto ack per leave game

--- game-service/src/modules/game/game.gateway.ts
+++ game-service/src/modules/game/game.gateway.ts
@@ -201,7 +201,13 @@ export class GameGateway
 			this.logger.warn(
 				`error in removing the player from the game, message: ${result.message}`,
 			);
-		} else this.logger.log(`client with socket-id ${socketId} is leaving the game`);
+		} else{
+			client.emit('LEAVE_GAME_ACK',{
+				status: 'success',
+				message: 'Partita abbandonata correttamente'
+			});
+			this.logger.log(`client with socket-id ${socketId} is leaving the game`);
+		}
 	}
 
 	@Throttle({ default: { limit: 20, ttl: 10000 } })
